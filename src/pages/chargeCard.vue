b79ccdbdecfc490ed4c30d903d22ede681559662
lisir <361028458@qq.com>
2023-10-08 18:10:55 +0800

    add

--- src/pages/chargeCard.vue
+++ src/pages/chargeCard.vue
@@ -1,75 +1,146 @@
 
 <template>
   <div class="movie-search-box">
-    <el-button @click="onclick">btn1</el-button>
-    <div class="selectedBorder" ref="box" v-if="bankSwitch">
-      <div class="bankItem">
-        你好我是弹窗里面的内容部分
-        </div>
-    </div>
+    <el-tree
+      ref="tree"
+      default-expand-all
+      :data="data"
+      @node-click="handleNodeClick"
+      node-key="id"
+      :current-node-key="currentNodeKey"
+      :highlight-current="true"
+      :props="defaultProps"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span :id="data.id">{{ node.label }}</span>
+      </span>
+    </el-tree>
+    <el-button @click="nodeClick1" type="primary">点击后选中id为006的</el-button>
+
   </div>
 </template>
 <script>
-const debounce = (function() {
-  let timer = 0;
-  return function(callback, ms = 200) {  //设置默认200ms
-    clearTimeout(timer);
-    timer = setTimeout(callback, ms);
-  };
-})();
-
 export default {
-  name: 'test',
-  data () {
+  data() {
     return {
-      bankSwitch:true,
-      isctive:'1'
-    }
-  },
-  mounted() {
-    document.addEventListener("click", this.bodyCloseMenus);
-  },
-    bodyCloseMenus(e) {
-        let self = this;
-          if (self.bankSwitch == true){
-            self.bankSwitch = false;
-          }
+      data: [
+        {
+          id: 1,
+          name: "一级 1",
+          children: [
+            {
+              id: 4,
+              name: "二级 1-1",
+              children: [
+                {
+                  id: 9,
+                  name: "三级 1-1-1",
+                },
+                {
+                  id: 10,
+                  name: "三级 1-1-2",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: "一级 2",
+          children: [
+            {
+              id: "005",
+              name: "二级 2-1",
+            },
+            {
+              id: "006",
+              name: "二级 2-2",
+            },
+          ],
+        },
+        {
+          id: 3,
+          name: "一级 3",
+          children: [
+            {
+              id: 7,
+              name: "二级 3-1",
+            },
+            {
+              id: 8,
+              name: "二级 3-2",
+              children: [
+                {
+                  id: 11,
+                  name: "三级 3-2-1",
+                },
+                {
+                  id: 12,
+                  name: "三级 3-2-2",
+                },
+                {
+                  id: 13,
+                  name: "三级 3-2-3",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      defaultProps: {
+        children: "children",
+        label: "name",
       },
-  beforeDestroy() {
-      document.removeEventListener("click", this.bodyCloseMenus);
+      currentNodeKey: "",
+    };
+  },
+   watch: {
+    currentNodeKey(newVal) {
+      if (newVal.toString()) {
+        this.$refs["tree"].setCurrentKey(newVal);
+      } else {
+        this.$refs["tree"].setCurrentKey(null);
+      }
     },
+  },
   methods: {
-    bodyCloseMenus(){
-      this.bankSwitch = false
-      // this.isctive = '2'
-      // console.log(this.isctive)
+    // 点击节点
+    handleNodeClick(data,node){
+      this.currentNodeKey = data.id;
+      // 判断点击的层级进行操作
+      if(node.level == 1){
+        console.log('第一层data1',data);
+        console.log('第一层node1',node);
+      }else if(node.level == 2){
+        console.log('第二层data2',data);
+        console.log('第二层node2',node);
+      }else if(node.level == 3){
+        console.log('第三层data3',data);
+        console.log('第三层node3',node);
+      }
     },
-    onclick(){
-      console.log('111')
-      // document.addEventListener("click", this.bodyCloseMenus);
-      this.bankSwitch = true
-      // this.isctive = '1'
-      console.log(this.bankSwitch)
-    },
-  //   debounce(fn, delay = 3000) {   //默认300毫秒
-  //       let timer;
-  //       return function () {
-  //       let args = arguments;
-  //       if (timer) {
-  //           clearTimeout(timer);
-  //       }
-  //       timer = setTimeout(() => {
-  //           fn.apply(this, args);   // this 指向vue
-  //       }, delay);
-  //       };
-  //   },
-  //    onclick(){
-  //        debounce(async () => {
-  //           console.log('111')
-  //         }, 5000);
 
-         
-  //    }
-    }
-}
+    nodeClick1(){
+      // 点击选中006号
+      this.$refs.tree.setCurrentKey('006'); 
+    },
+
+    // 如果数据过多可以调用这个方法并传入要显示的id
+    //滚动到选中定位的位置
+    selectedRegion(id) {
+      console.log("滚动到选中定位的位置");
+      // 通过Id获取到对应的dom元素
+      const node = document.getElementById(id);
+      setTimeout(() => {
+        if (node) {
+          this.$nextTick(() => {
+            // 通过scrollIntoView方法将对应的dom元素定位到可见区域 【block: 'center'】这个属性是在垂直方向居中显示
+            node.scrollIntoView({ block: "center" });
+          });
+        }
+      }, 100);
+    },
+  },
+};
 </script>
+
--- src/pages/upload4.vue
+++ src/pages/upload4.vue
@@ -1,42 +1,73 @@
 <template>
     <div>
-        <img v-for="(item,index) in imgarr"
-        width="100"
-        height="100"
-        :src="item" 
-        :key="index"
-        @click="aaa(imgarr,index)"
-        />
+        <el-upload
+   ref="upload"
+   action="index.php"
+   accept=".jpg,.jpeg,.png"
+   list-type="picture-card"
+   :class="{ 'none-up' : uploadDisabled }"
+   :on-preview="handlePictureCardPreview"
+   :on-remove="handleRemove"
+   :http-request="uploadImage"
+   :on-success="handleUploadPatternListSuccess"
+   :file-list="infoForm.effect"
+   :limit="3"
+   :auto-upload="true"
+ >
+   <i class="el-icon-plus" />
+ </el-upload>
+ 
+ <!-- 预览弹出框-->
+ <el-dialog :visible.sync="dialogVisible" append-to-body>
+   <img width="100%" :src="dialogImageUrl" alt="">
+ </el-dialog>
+
     </div>
 </template>
 
 <script>
-import { ImagePreview,Image } from 'vant';
 export default {
-    components: {
-        [ImagePreview.Component.name]: ImagePreview.Component,
-        // [Image.Component.name]: Image.Component,
-    },
+    
     data(){
         return{
-            imgarr:[]
-            // 'https://img01.yzcdn.cn/vant/cat.jpeg'
+            dialogImageUrl:'',
+            uploadDisabled:'',
+            infoForm:[]
         }
     },
     mounted(){
-        console.log()
-        this.imgarr = [JSON.parse(sessionStorage.getItem('setimg')),JSON.parse(sessionStorage.getItem('setimge'))]
+        
     },
     methods:{
-        aaa(imgarr,index){
-            // console.log(row)
-            ImagePreview({
-                images: imgarr,
-                showIndex: true,
-                loop: false,
-                startPosition: index
-            });
-        }
+        
+    // 上传图片的方法
+    uploadImage(res) {
+    const formData = new FormData()
+    formData.append('file', res.file)
+    upload(formData).then((resData) => {
+        this.fileList.push({ url: resData.data })
+        this.uploadDisabled = this.fileList.length >= 3
+    })
+    },
+    // 移除图片功能
+    handleRemove(file, fileList) {
+    const index = this.fileList.findIndex(item => {
+        return item.uid === file.uid
+    })
+    this.fileList.splice(index, 1)
+    this.uploadDisabled = this.fileList.length >= 3
+    },
+    // 解决上传时图片跳动问题
+    handleUploadPatternListSuccess(response, file, fileList) {
+    this.infoForm.effect = fileList
+    },
+    // 预览图片功能
+    handlePictureCardPreview(file) {
+        console.log(file)
+    this.dialogVisible = true
+    this.dialogImageUrl = file.url
+    },
+
     }
 }
 </script>
