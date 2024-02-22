be9adac8b2bc0f14f6977285afabfdedeb8f1c70
li361028458 <361028458@qq.com>
2024-02-22 11:12:24 +0800

    add

--- src/pages/maodian.vue
+++ src/pages/maodian.vue
@@ -1,187 +1,168 @@
 <template>
-  <div>
-    <el-menu
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-      :default-active="activeIndex"
-      router
-    >
-      <NavMenu :navMenus="menuData"></NavMenu>
-    </el-menu>
-    <router-view></router-view>
-  </div>
+	<div>
+    <template>
+   <div class="info-body">
+      <!--锚点列表-->
+      <ul class="archor">
+        <li :class="activeStep === anchor.id ? 'active' : ''"
+            v-for="(anchor, index) in anchorList"
+            :key="index" @click="jump(anchor.id)">
+          <span>{{ anchor.name }}</span>
+        </li>
+      </ul>
+ 
+      <!--隐藏滚动条-->
+      <div class="hid-scroll-bar">
+        <div class="scroll-box" id="scroll-box">
+ 
+          <!--模块列表-->
+          <div v-for="(item, index) in modules"
+               class="info-panel"
+               :key="index"
+               :id="item.id">
+            <div class="info-content">
+              <!--模块内容-->
+            </div>
+          </div>
+ 
+        </div>
+      </div>
+ 
+   </div>
 </template>
-
+	</div>
+</template>
 <script>
-import NavMenu from "@/components/NavMenu.vue";
+// import TitleAnchor from "../components/TitleAnchor.vue";
 export default {
-  components: {
-    NavMenu: NavMenu
-  },
-  data() {
+   components: {
+      // TitleAnchor,
+   },
+   data() {
     return {
-      activeIndex: 'aa',
-      menuData: [
-        {
-          //一级
-          entity: {
-            id: 0,
-            name: "aa",
-            icon: "el-icon-message",
-            alias: "一级菜单"
-          }
-        },
-        {
-          //一级
-          entity: {
-            id: 1,
-            name: "systemManage",
-            icon: "el-icon-message",
-            alias: "两级菜单"
-          },
-          //二级
-          childs: [
-            {
-              entity: {
-                id: 3,
-                name: "authManage",
-                icon: "el-icon-loading",
-                alias: "权限管理",
-                value: { path: "/hello" }
-              }
-            },
-            {
-              entity: {
-                id: 4,
-                name: "roleManage",
-                icon: "el-icon-bell",
-                alias: "角色管理",
-                value: "/system/role"
-              }
-            },
-            {
-              entity: {
-                id: 2,
-                name: "menuManage",
-                icon: "el-icon-edit",
-                alias: "菜单管理",
-                value: "/system/menu"
-              }
-            },
-            {
-              entity: {
-                id: 5,
-                name: "groupManage",
-                icon: "el-icon-mobile-phone\r\n",
-                alias: "分组管理",
-                value: "/system/group"
-              }
-            }
-          ]
-        },
-        {
-          //一级
-          entity: {
-            id: 6,
-            name: "userManage",
-            icon: "el-icon-news",
-            alias: "三级菜单"
-          },
-          //二级
-          childs: [
-            {
-              entity: {
-                id: 7,
-                name: "accountManage",
-                icon: "el-icon-phone-outline\r\n",
-                alias: "帐号管理",
-                value: ""
-              },
-              //三级
-              childs: [
-                {
-                  entity: {
-                    id: 14,
-                    name: "emailManage",
-                    icon: "el-icon-sold-out\r\n",
-                    alias: "邮箱管理",
-                    value: "/content/email"
-                  }
-                },
-                {
-                  entity: {
-                    id: 13,
-                    name: "passManage",
-                    icon: "el-icon-service\r\n",
-                    alias: "密码管理",
-                    value: "/content/pass"
-                  }
-                }
-              ]
-            },
-            {
-              entity: {
-                id: 8,
-                name: "integralManage",
-                icon: "el-icon-picture",
-                alias: "积分管理",
-
-                value: "/user/integral"
-              }
-            }
-          ]
-        },
-        {
-          //一级
-          entity: {
-            id: 40,
-
-            name: "contentManage",
-            icon: "el-icon-rank",
-            alias: "四级菜单"
-          },
-          //er级
-          childs: [
-            {
-              entity: {
-                id: 41,
-                name: "classifyManage2",
-                icon: "el-icon-printer",
-                alias: "分类管理"
-              },
-              //三级
-              childs: [
-                {
-                  entity: {
-                    id: 42,
-                    name: "classifyManage3",
-                    icon: "el-icon-printer",
-                    alias: "分类管理"
-                  },
-                  //四级
-                  childs: [
-                    {
-                      entity: {
-                        id: 43,
-                        name: "classifyManage4",
-                        icon: "el-icon-printer",
-                        alias: "分类管理",
-                        value: "/content/classify"
-                      }
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
+      activeStep: '',// 默认选中的锚点的key值
+      offsetTop: 0,
+    }
+  },
+  computed: {
+    scrollFn() { // 防抖
+      return _.debounce(
+        this.scroll, 100
+      )
+    },
+    anchorList() { // 锚点列表
+      return [
+       { id: 'anchor1', name: '锚点1' },
+       { id: 'anchor2', name: '锚点2' },
+       { id: 'anchor3', name: '锚点3' },
+      ];
+    },
+    modules() { // 模块列表
+      return [
+        { id: 'anchor1', title: '模块1' },
+        { id: 'anchor2', title: '模块2' },
+        { id: 'anchor3', title: '模块3' },
+      ];
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.scrollFn, true)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.scrollFn, false)
+  },
+  methods: {
+    scroll() {
+      const box = document.getElementById('scroll-box')
+      // 若当前设置的滚动高度大于实际滚动的高度，即为锚点跳转，不再设置选中的锚点
+      if(this.offsetTop > box.scrollTop) {
+        this.offsetTop = 0
+        return
+      }
+      let totalH = 0
+      this.anchorList.some(anchor=> {
+        let scrollItem = document.getElementById(anchor.id)// 锚点对应的模块
+        totalH = totalH + scrollItem.clientHeight
+        let judge = box.scrollTop < totalH
+        if(judge) {
+          this.activeStep = anchor.id
+          return true
         }
-      ]
-    };
+      })
+    },
+    jump(id) {
+      this.activeStep = id // 设置选中的锚点为当前点击的
+      const box = document.getElementById('scroll-box')
+      let scrollItem = document.getElementById(id)
+      // 锚点对应的模块与最近的一个具有定位的祖宗元素顶部的距离
+      this.offsetTop = scrollItem.offsetTop
+      box.scrollTo({
+        top: scrollItem.offsetTop,
+        behavior: "smooth",
+      });
+    },
+ 
   }
-};
+}
 </script>
+<style scoped> /*锚点*/
+.archor {
+  position: absolute;
+  right: 16px;
+  display: flex;
+  flex-direction: column;
+  width: 40px;
+  list-style: none;
 
-<style>
+  
+
+}
+li {
+    height: 80px;
+    border-right: 3px solid #FAFAFA;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    cursor: pointer;
+
+    
+  }
+  li:first-child {
+      justify-content: flex-start;
+    }
+
+    li:last-child {
+      justify-content: flex-end;
+    }
+
+    li.active {
+      border-color: #ddd;
+    }
+.info-body {
+  position: relative;
+  width: 65%;
+  border-left: 2px solid #F2F2F2;
+  padding: 2px 82px 0 0;
+  box-sizing: border-box;
+}
+
+/*双重包裹，隐藏滚动条*/
+.hid-scroll-bar {
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  
+}
+.scroll-box {
+    height: 100%;
+    width: 100%;
+    overflow-y: auto;
+    position: absolute;
+
+    
+  }
+  .scroll-box::-webkit-scrollbar {
+      display: none;
+    }
 </style>
-
